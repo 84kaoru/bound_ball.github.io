a46902723744916c312d3db21ce9055441a19559
morikawa kaoru <128456055+84kaoru@users.noreply.github.com>
2024-03-08 16:11:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var ballRadius = 10;
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x,y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "blue"; 
+    ctx.fillStyle = "#0095DD"; 
     ctx.fill();
     ctx.closePath;
 }
