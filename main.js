b50832423cf154c96d1960348f7b247c490c3636
morikawa kaoru <128456055+84kaoru@users.noreply.github.com>
2024-03-08 16:03:56 +0900

    ボールバウンド

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+
+// ボールの初期位置(x,y) ボールの動き(dx.dy) ボールの半径(ballRadius)
+
+var x = canvas.width /2;
+var y = canvas.height - 30;
+var dx = 2;
+var dx = -2;
+var ballRadius = 10;
+
+// HTMLキャンパスでは座標の原点はキャンバスの左上の端
+// ボールの動き　X>0なら　ボールは右
+//             Y>0なら　"ボールは下"
+
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "blue"; 
+    ctx.fill();
+    ctx.closePath;
+}
